0783c9539997582cd9d29b543f3f013d8ae83b60
Ariel Rey <ariel.rey@openenglish.com>
2014-04-03 11:54:56 -0300

    Changing to list on match repository

--- src/main/java/com/prode/repo/MatchRepository.java
+++ src/main/java/com/prode/repo/MatchRepository.java
@@ -1,6 +1,7 @@
 package com.prode.repo;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,9 +15,9 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
 	Match findOne(Long id);
 	
     @Query("select m from Match m where lower(teamA) = lower(:teamA) and lower(teamB) = lower(:teamB)")
-	Match findByMatchTeams(@Param("teamA") Team teamA, @Param("teamB") Team teamB);
+    List<Match> findByMatchTeams(@Param("teamA") Team teamA, @Param("teamB") Team teamB);
 	
     @Query("select m from Match m where dateMatch = :dateMatch")
-    Match findByMatchDate(@Param("dateMatch") Date date);
+    List<Match> findByMatchDate(@Param("dateMatch") Date date);
    
 }
