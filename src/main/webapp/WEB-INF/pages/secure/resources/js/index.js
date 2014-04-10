b621f3376de65a19ea62bf31bc8da83d4f2e06fb
Ariel Rey <ariel.rey@openenglish.com>
2014-04-09 22:33:50 -0300

    Adding points calculation

--- src/main/webapp/WEB-INF/pages/secure/resources/js/index.js
+++ src/main/webapp/WEB-INF/pages/secure/resources/js/index.js
@@ -7,25 +7,32 @@ checkResults = function(){
 	var groups = $('#prode-container .pr-index');
 	
 	$(groups).each(function(key, value){
-		var results = $(value).find('td.results');
-		console.log('run')
+		var results = $(value).find('td.results'),
+			puntos = 0;
+		
 		$(results).each(function(key, value){
 			var result_a = $(value).find('input.team_a_result'),
 				result_b = $(value).find('input.team_b_result'),
 				result_a_fifa = $(value).find('input.team_a_result_fifa'),
 				result_b_fifa = $(value).find('input.team_b_result_fifa'),
-				partido = false;
+				partido = false,
+				pronostico = false;
+				golCorrecto = false;
 			
 			if( $(result_a_fifa).val() != '' ){
 				//Check only the goals
 				if( $(result_a).val() == $(result_a_fifa).val() ){
 					$(result_a).addClass('equal');
+					
+					golCorrecto = true;
 				}else{
 					$(result_a).addClass('notequal');
 				}
 				
 				if( $(result_b).val() == $(result_b_fifa).val() ){
 					$(result_b).addClass('equal');
+					
+					golCorrecto = true;
 				}else{
 					$(result_b).addClass('notequal');
 				}
@@ -40,17 +47,36 @@ checkResults = function(){
 				if( !partido ){
 					if( ($(result_a_fifa).val() > $(result_b_fifa).val()) && ($(result_a).val() > $(result_b).val()) ){
 						$(value).parents('tr').addClass('info');
+						
+						pronostico = true;
 					}
 					
 					if( ($(result_a_fifa).val() < $(result_b_fifa).val()) && ($(result_a).val() < $(result_b).val()) ){
 						$(value).parents('tr').addClass('info');
+						
+						pronostico = true;
 					}
 					
 					if( ($(result_a_fifa).val() == $(result_b_fifa).val()) && ($(result_a).val() == $(result_b).val()) ){
 						$(value).parents('tr').addClass('info');
+						
+						pronostico = true;
 					}
 				}
+				
+				//Calculo de puntos
+				if( partido == true ){
+					puntos = puntos + 5;
+				}else if( pronostico == true && golCorrecto == true ){
+					puntos = puntos + 3;
+				}else if( pronostico == true ){
+					puntos = puntos + 2;
+				}else if( golCorrecto == true ){
+					puntos = puntos + 1;
+				}
 			}
-		})
+		});
+		
+		$(value).find('.panel-heading').append('<div class="points">Points ' + puntos + '</div>')
 	})
 }
