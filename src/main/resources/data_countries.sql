bf38ebfb897739c040f448d46a292a6e115e8c7f
Maximiliano Micciullo <maximiliano.micciullo@openenglish.com>
2014-04-11 09:43:21 -0300

    data countries and sectors

---
+++ src/main/resources/data_countries.sql
@@ -0,0 +1,4 @@
+INSERT INTO countries (id, name) VALUES (1, 'Argentina');
+INSERT INTO countries (id, name) VALUES (2, 'Brasil');
+INSERT INTO countries (id, name) VALUES (3, 'Venezuela');
+INSERT INTO countries (id, name) VALUES (4, 'Usa');
