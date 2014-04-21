1becf422df74d47546fc477b54b8641340fd0378
Maximiliano Micciullo <maximiliano.micciullo@openenglish.com>
2014-04-21 00:47:14 -0300

    job quartz

---
+++ src/main/java/com/prode/jobs/MyTask.java
@@ -0,0 +1,12 @@
+package com.prode.jobs;
+
+import java.util.Calendar;
+
+public class MyTask {
+
+	public void printCurrentTime() {  
+		// printing current system time  
+		System.out.println("Current Time : " + Calendar.getInstance().getTime());  
+	}  
+
+}
